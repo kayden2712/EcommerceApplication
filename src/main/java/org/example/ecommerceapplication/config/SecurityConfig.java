20f7b683513a3277d5d70d9bcb75afe6658d5bf5
ĐỖ VŨ TÙNG <tung74dcht22118@st.utt.edu.vn>
2026-02-12 18:30:57 +0700

    thiet lam security

--- src/main/java/org/example/ecommerceapplication/config/SecurityConfig.java
+++ src/main/java/org/example/ecommerceapplication/config/SecurityConfig.java
@@ -1,27 +1,36 @@
 package org.example.ecommerceapplication.config;
 
+import org.example.ecommerceapplication.security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+
     @Bean
+    @SuppressWarnings("unused")
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-        return http.build();
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable()) // Disable CSRF for stateless APIs
+                .httpBasic(httpBasic -> httpBasic.disable()) // Disable HTTP Basic authentication
+                .formLogin(formLogin -> formLogin.disable()) // Disable form-based login
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Stateless session management
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // Temporary: Allow all requests without authentication
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class) // Add JWT filter
+                .build();
     }
 
 }
