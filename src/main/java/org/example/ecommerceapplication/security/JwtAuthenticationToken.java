81bd8508331cb7482d5630ebb9c040596858af82
ĐỖ VŨ TÙNG <tung74dcht22118@st.utt.edu.vn>
2026-02-12 10:50:52 +0700

    xoa comment

--- src/main/java/org/example/ecommerceapplication/security/JwtAuthenticationToken.java
+++ src/main/java/org/example/ecommerceapplication/security/JwtAuthenticationToken.java
@@ -1,11 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package org.example.ecommerceapplication.security;
 
 import java.util.Collection;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
